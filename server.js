8666bada5a5aec47e3734ea40107ecba4f5134d8
sathiyamoorthy31 <34150737+sathiyamoorthy31@users.noreply.github.com>
2018-03-07 12:57:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -53,6 +54,29 @@ var htmltemplate =`
 `;
    return htmltemplate;
 }
+
+function hash (input,salt){
+    //how do we create hash?
+    var hashed = crypto.pbkdf2sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+    
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
